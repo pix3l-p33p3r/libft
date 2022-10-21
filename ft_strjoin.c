d9f408ba9364d8bb5067db3992a1f3d907d7a12e
El houcine  El yakoubi <elel-yak@e2r6p8.1337.ma>
2022-10-21 15:43:17 +0100

    adding itoa

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: elel-yak <elel-yak@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/17 18:22:12 by elel-yak          #+#    #+#             */
-/*   Updated: 2022/10/18 18:38:39 by elel-yak         ###   ########.fr       */
+/*   Updated: 2022/10/21 15:16:57 by elel-yak         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,14 @@
 
 char	*ft_strjoin(char const *s1, char const *s2)
 {
-	
+	char	*result;
+
+	if (!s1 || !s2)
+		return (NULL);
+	result = malloc(ft_strlen(s1) + ft_strlen(s2) + 1);
+	if (!result)
+		return (NULL);
+	ft_strlcpy(result, s1, ft_strlen(s1) + 1);
+	ft_strlcat(result, s2, ft_strlen(s1) + ft_strlen(s2) + 1);
+	return (result);
 }
